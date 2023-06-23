aa0a2c1e71d149f89edcfde29622e0e412dcb9f4
Ajay Pawar <65480235+Ajaypawar02@users.noreply.github.com>
2023-06-23 13:29:59 +0530

    Update test.py

--- app/test.py
+++ app/test.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0,'./Animations/temp/Animations')
 sys.path.insert(1,'./Animations/temp/Animations/examples')
 sys.path.insert(2,'./Animations/temp/Animations/TextExtraction-to-voice')
-from examples.image_to_animation import image_to_animation
+from image_to_animation import image_to_animation
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from animated_drawings import render
