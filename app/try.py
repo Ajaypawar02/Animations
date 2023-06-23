dab82af4aa1b4b26f564340ed78c38d7095a9fec
Ajay Pawar <65480235+Ajaypawar02@users.noreply.github.com>
2023-06-23 12:34:01 +0530

    Update try.py

--- app/try.py
+++ app/try.py
@@ -1,13 +1,5 @@
-import yaml
+import animated_drawings.render
 
-# Load the YAML file
-with open('/home/ajay/Animations/AnimatedDrawings/config/retarget/fair1_ppf_duo1.yaml', 'r') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
-
-# Update the char_starting_location
-DIM = [0.6, 0, -0.6]  # New values for char_starting_location
-data['char_starting_location'] = DIM
-
-# Save the updated YAML file
-with open('updated_yaml_file.yaml', 'w') as file:
-    yaml.dump(data, file)
+if __name__ == "__main__":
+    animated_drawings.render.start('./examples/config/mvc/export_gif_example.yaml')
+    animated_drawings.render.start('./examples/config/mvc/export_gif_example.yaml')
